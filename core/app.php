c92b32c48e55e9ce57d288d2c1df50e63530cf24
chuckiezhou <zzp208@qq.com>
2018-10-31 18:17:45 +0800

    初始化admin

---
+++ core/app.php
@@ -0,0 +1,16 @@
+<?php
+
+class App{
+	/**
+	 * 项目主目录
+	 * @var string
+	 */
+	public static $path;
+
+	/**
+	 * 项目环境 开发:dev 测试:test 产品:product
+	 * @var string
+	 */
+	public static $env;
+
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+define('APP_ENV', 'dev');
+define('APP_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+require(PROJECT_PATH . 'core/app.php');
