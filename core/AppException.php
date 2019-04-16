5bd38f3e8f299d5e7a14a27b955792aa8959afd3
chuckiechou <zzp208@qq.com>
2019-04-16 15:18:17 +0800

    add Log,AppException

---
+++ core/AppException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace zerg\core;
+
+
+class AppException{
+
+}
---
+++ core/Log.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/4/16 0016
+ * Time: 下午 3:16
+ */
+
+namespace zerg\core;
+
+
+class Log{
+
+}
